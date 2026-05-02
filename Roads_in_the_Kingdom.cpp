08b5efcde6faf43db37188b8bfac83d5e8e67414
Sahil Singh <lcs2021033@iiitl.ac.in>
2026-05-02 12:59:13 +0530

    Completed Sterling number of second kind + partial game theory

---
+++ Roads_in_the_Kingdom.cpp
@@ -0,0 +1,466 @@
+// Author : Sahil Singh
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("O3,unroll-loops")
+
+#define ll long long
+#define vll vector<long long>
+#define vi vector<int>
+#define vpll vector<pair<long long,long long>>
+#define mll map<long long,long long>
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define vvll vector<vector<long long>>
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#define all(v) (v).begin(),(v).end()
+#define rep(i,k,n) for(ll i=k;i<n;i++)
+#define repr(i,k,n) for(ll i=k;i>=n;i--)
+
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define endl "\n"
+#define mod 1000000007
+
+#define fast \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr); \
+    cout.tie(nullptr);
+
+template<typename... T>
+void read(T&... args){ ((cin>>args),...); }
+
+template<typename... T>
+void print(T... args){
+    ((cout<<args<<" "),...);
+    cout<<endl;
+}
+
+#define invll(x,n) \
+    vll x(n); \
+    rep(i,0,n) cin>>x[i];
+#define invi(x,n) \
+    vi x(n); \
+    rep(i,0,n) cin>>x[i];
+#define inarrll(x,n) \
+    ll x[n]; \
+    rep(i,0,n) cin>>x[i];
+#define inarrint(x,n) \
+    int x[n]; \
+    rep(i,0,n) cin>>x[i];
+
+#define instr(...) \
+    string __VA_ARGS__; \
+    read(__VA_ARGS__);
+#define inll(...) \
+    ll __VA_ARGS__; \
+    read(__VA_ARGS__);
+#define inint(...) \
+    int __VA_ARGS__; \
+    read(__VA_ARGS__);
+
+#define debug(arg) cerr<<__LINE__<<" "<<#arg<<" --> "<<arg<<endl;
+#define clr(a) memset(a,0,sizeof(a))
+#define lb lower_bound
+#define ub upper_bound
+#define setbits(x) __builtin_popcount(x)
+#define setbitsll(x) __builtin_popcountll(x)
+
+inline ll addmod(ll a,ll b){ a+=b; if(a>=mod) a-=mod; return a; }
+inline ll submod(ll a,ll b){ a-=b; if(a<0) a+=mod; return a; }
+inline ll mulmod(ll a,ll b){ return (a%mod)*(b%mod)%mod; }
+
+ll binExp(ll a,ll e,ll m=mod){
+    a%=m;
+    ll r=1;
+    while(e){
+        if(e&1) r=(r*a)%m;
+        a=(a*a)%m;
+        e>>=1;
+    }
+    return r;
+}
+
+ll modInv(ll a,ll m=mod){ return binExp(a,m-2,m); }
+
+ll gcdll(ll a,ll b){
+    while(b){
+        a%=b;
+        swap(a,b);
+    }
+    return a;
+}
+
+ll lcmll(ll a,ll b){
+    return a/gcdll(a,b)*b;
+}
+
+vll fact,invFact;
+
+void combi(int n){
+    fact.assign(n+1,1);
+    invFact.assign(n+1,1);
+    rep(i,1,n+1) fact[i]=mulmod(fact[i-1],i);
+    invFact[n]=modInv(fact[n]);
+    for(int i=n-1;i>=0;i--){
+        invFact[i]=mulmod(invFact[i+1],i+1);
+    }
+}
+
+ll ncr(ll n,ll r){
+    if(r<0||r>n) return 0;
+    return mulmod(fact[n],mulmod(invFact[r],invFact[n-r]));
+}
+
+vector<int> primes,lp;
+
+void sieve(int n=2000000){
+    lp.assign(n+1,0);
+    primes.clear();
+    for(int i=2;i<=n;++i){
+        if(!lp[i]){
+            lp[i]=i;
+            primes.pb(i);
+        }
+        for(int p:primes){
+            if(p>lp[i]||1LL*i*p>n) break;
+            lp[i*p]=p;
+        }
+    }
+}
+
+struct BIT{
+    int n;
+    vector<ll> bit;
+
+    BIT(int _=0){ init(_); }
+
+    void init(int _){
+        n=_;
+        bit.assign(n+1,0);
+    }
+
+    void add(int idx,ll val){
+        for(++idx;idx<=n;idx+=idx&-idx) bit[idx]+=val;
+    }
+
+    ll sumPrefix(int idx){
+        ll s=0;
+        for(++idx;idx;idx-=idx&-idx) s+=bit[idx];
+        return s;
+    }
+
+    ll sumRange(int l,int r){
+        return sumPrefix(r)-(l?sumPrefix(l-1):0);
+    }
+};
+
+struct DSU{
+    vector<int> p,sz;
+
+    DSU(int n=0){ init(n); }
+
+    void init(int n){
+        p.resize(n);
+        iota(all(p),0);
+        sz.assign(n,1);
+    }
+
+    int find(int x){
+        return p[x]==x?x:p[x]=find(p[x]);
+    }
+
+    bool unite(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a==b) return false;
+        if(sz[a]<sz[b]) swap(a,b);
+        p[b]=a;
+        sz[a]+=sz[b];
+        return true;
+    }
+};
+
+template<typename T>
+struct SegTree{
+    int n;
+    vector<T> st;
+    T id;
+    function<T(T,T)> op;
+
+    SegTree(int _n,T _id,function<T(T,T)> _op):n(_n),id(_id),op(_op){
+        st.assign(2*n,id);
+    }
+
+    void build(const vector<T>& a){
+        rep(i,0,n) st[i+n]=a[i];
+        for(int i=n-1;i;--i) st[i]=op(st[i<<1],st[i<<1|1]);
+    }
+
+    void upd(int pos,T val){
+        for(st[pos+=n]=val;pos>1;pos>>=1) st[pos>>1]=op(st[pos],st[pos^1]);
+    }
+
+    T query(int l,int r){
+        T L=id,R=id;
+        for(l+=n,r+=n+1;l<r;l>>=1,r>>=1){
+            if(l&1) L=op(L,st[l++]);
+            if(r&1) R=op(st[--r],R);
+        }
+        return op(L,R);
+    }
+};
+
+struct Matrix{
+    int n;
+    vvll a;
+
+    Matrix(int _n=0,bool id=false):n(_n),a(n,vector<ll>(n,0)){
+        if(id) rep(i,0,n) a[i][i]=1;
+    }
+
+    Matrix operator*(const Matrix& o)const{
+        Matrix r(n);
+        rep(i,0,n){
+            rep(k,0,n){
+                if(a[i][k]){
+                    rep(j,0,n){
+                        r.a[i][j]=(r.a[i][j]+a[i][k]*o.a[k][j])%mod;
+                    }
+                }
+            }
+        }
+        return r;
+    }
+};
+
+Matrix matPow(Matrix base,ll exp){
+    Matrix res(base.n,true);
+    while(exp){
+        if(exp&1) res=res*base;
+        base=base*base;
+        exp>>=1;
+    }
+    return res;
+}
+
+struct custom_hash{
+    static uint64_t splitmix64(uint64_t x){
+        x+=0x9e3779b97f4a7c15;
+        x=(x^(x>>30))*0xbf58476d1ce4e5b9;
+        x=(x^(x>>27))*0x94d049bb133111eb;
+        return x^(x>>31);
+    }
+
+    size_t operator()(uint64_t x)const{
+        static const uint64_t FIXED_RANDOM=chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x+FIXED_RANDOM);
+    }
+};
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const vector<pair<int,int>> dir={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+
+ll mergeMin(ll a,ll b){ return min(a,b); }
+ll mergeMax(ll a,ll b){ return max(a,b); }
+
+void buildMin(vll& arr,ll start,ll end,ll index,ll seg[]){
+    if(start==end){
+        seg[index]=arr[start];
+        return;
+    }
+    ll mid=(start+end)/2;
+    buildMin(arr,start,mid,2*index+1,seg);
+    buildMin(arr,mid+1,end,2*index+2,seg);
+    seg[index]=mergeMin(seg[2*index+1],seg[2*index+2]);
+}
+
+void buildMax(vll& arr,ll start,ll end,ll index,ll seg[]){
+    if(start==end){
+        seg[index]=arr[start];
+        return;
+    }
+    ll mid=(start+end)/2;
+    buildMax(arr,start,mid,2*index+1,seg);
+    buildMax(arr,mid+1,end,2*index+2,seg);
+    seg[index]=mergeMax(seg[2*index+1],seg[2*index+2]);
+}
+
+void updateMin(ll pos,ll val,ll start,ll end,ll index,ll seg[]){
+    if(start==end){
+        seg[index]=val;
+        return;
+    }
+    ll mid=(start+end)/2;
+    if(pos<=mid) updateMin(pos,val,start,mid,2*index+1,seg);
+    else updateMin(pos,val,mid+1,end,2*index+2,seg);
+    seg[index]=mergeMin(seg[2*index+1],seg[2*index+2]);
+}
+
+void updateMax(ll pos,ll val,ll start,ll end,ll index,ll seg[]){
+    if(start==end){
+        seg[index]=val;
+        return;
+    }
+    ll mid=(start+end)/2;
+    if(pos<=mid) updateMax(pos,val,start,mid,2*index+1,seg);
+    else updateMax(pos,val,mid+1,end,2*index+2,seg);
+    seg[index]=mergeMax(seg[2*index+1],seg[2*index+2]);
+}
+
+ll queryMin(ll l,ll r,ll start,ll end,ll index,ll seg[]){
+    if(r<start||end<l) return LLONG_MAX;
+    if(l<=start&&end<=r) return seg[index];
+    ll mid=(start+end)/2;
+    return mergeMin(
+        queryMin(l,r,start,mid,2*index+1,seg),
+        queryMin(l,r,mid+1,end,2*index+2,seg)
+    );
+}
+
+ll queryMax(ll l,ll r,ll start,ll end,ll index,ll seg[]){
+    if(r<start||end<l) return LLONG_MIN;
+    if(l<=start&&end<=r) return seg[index];
+    ll mid=(start+end)/2;
+    return mergeMax(
+        queryMax(l,r,start,mid,2*index+1,seg),
+        queryMax(l,r,mid+1,end,2*index+2,seg)
+    );
+}
+
+ll n;
+vector<vpll> adj;
+vector<bool> vis;
+
+ll diam = 0;
+ll dfs(ll root, ll p){
+    ll maxi=0,maxi2 = 0;
+    for(auto &edge:adj[root]){
+        ll v = edge.fi;
+        ll w = edge.se;
+        if(v==p || vis[v]){
+            continue;
+        }
+        ll depth = dfs(v,root)+w;
+        if(depth>maxi){
+            maxi2 = maxi;
+            maxi=depth;
+        }
+        else if(depth>maxi2){
+            maxi2 = depth;
+        }
+    }
+    diam = max(diam,maxi+maxi2);
+    return maxi;
+}
+
+void solve(){
+    cin>>n;
+    adj.assign(n+1,vpll());
+    vll deg(n+1);
+    rep(i,0,n){
+        inll (u,v,w);
+        adj[u].pb({v,w});
+        adj[v].pb({u,w});
+        deg[u]++;
+        deg[v]++;
+    }
+    queue<ll> q;
+    rep(i,0,n+1){
+        if(deg[i]==1){
+            q.push(i);
+        }
+    }
+    vis.assign(n+1,true);
+    while(!q.empty()){
+        ll u =q.front();
+        q.pop();
+        vis[u] = false;
+        for(auto &edge:adj[u]){
+            ll v = edge.fi;
+            deg[v]--;
+            if(deg[v]==1){
+                q.push(v);
+            }
+        }
+    }
+    ll start = -1;
+    rep(i, 1, n + 1){
+        if(vis[i]){
+            start = i; 
+            break; 
+        }
+    }
+    vll nodes,edges;
+    ll curr = start, p = -1;
+    while(true) {
+        nodes.pb(curr);
+        int nxt = -1;
+        ll wt = -1;
+        for(auto& edge:adj[curr]) {
+            int v = edge.fi;
+            ll w = edge.se;
+            if(vis[v] && v != p) {
+                nxt = v;
+                wt = w;
+                break;
+            }
+        }
+        if(nxt==start){
+            edges.pb(wt);
+            break;
+        }
+        edges.pb(wt);
+        p = curr;
+        curr = nxt;
+    }
+    ll k = nodes.size();
+    vll H(k), D(k);
+    rep(i, 0, k) {
+        diam = 0;
+        H[i] = dfs(nodes[i], -1);
+        D[i] = diam;
+    }
+    vll W(k, 0);
+    rep(i, 1, k){
+        W[i] = W[i-1] + edges[i-1];
+    }
+    ll L = W[k-1] + edges[k-1];
+    vll pref(k), pref1(k), prefmax(k);
+    pref[0] = H[0] + W[0];
+    pref1[0] = H[0] - W[0];
+    prefmax[0] = D[0];
+    rep(i, 1, k) {
+        prefmax[i] = max({prefmax[i-1], D[i], H[i] + W[i] + pref1[i-1]});
+        pref[i] = max(pref[i-1], H[i] + W[i]);
+        pref1[i] = max(pref1[i-1], H[i] - W[i]);
+    }
+    vll suff(k), suff1(k), suffmax(k);
+    suff[k-1] = H[k-1] + W[k-1];
+    suff1[k-1] = H[k-1] - W[k-1];
+    suffmax[k-1] = D[k-1];
+    for(int i=k-2; i>=0; i--) {
+        suffmax[i] = max({suffmax[i+1], D[i], H[i] - W[i] + suff[i+1]});
+        suff[i] = max(suff[i+1], H[i] + W[i]);
+        suff1[i] = max(suff1[i+1], H[i] - W[i]);
+    }
+    ll mini = prefmax[k-1];    
+    rep(i, 0, k - 1) {
+        ll cur_dia = max({prefmax[i], suffmax[i+1], pref[i] + suff1[i+1] + L});
+        mini = min(mini, cur_dia);
+    }
+    print(mini);
+
+}
+
+int main(){
+    fast;
+    solve();
+    return 0;
+}
---
+++ dominant_indices.cpp
@@ -0,0 +1,393 @@
+// Author : Sahil Singh
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma GCC optimize("O3,unroll-loops")
+
+#define ll long long
+#define vll vector<long long>
+#define vi vector<int>
+#define vpll vector<pair<long long,long long>>
+#define mll map<long long,long long>
+#define pii pair<int,int>
+#define pll pair<long long,long long>
+#define vvll vector<vector<long long>>
+
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+
+#define all(v) (v).begin(),(v).end()
+#define rep(i,k,n) for(ll i=k;i<n;i++)
+#define repr(i,k,n) for(ll i=k;i>=n;i--)
+
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define endl "\n"
+#define mod 1000000007
+
+#define fast \
+    ios::sync_with_stdio(false); \
+    cin.tie(nullptr); \
+    cout.tie(nullptr);
+
+template<typename... T>
+void read(T&... args){ ((cin>>args),...); }
+
+template<typename... T>
+void print(T... args){
+    ((cout<<args<<" "),...);
+    cout<<endl;
+}
+
+#define invll(x,n) \
+    vll x(n); \
+    rep(i,0,n) cin>>x[i];
+#define invi(x,n) \
+    vi x(n); \
+    rep(i,0,n) cin>>x[i];
+#define inarrll(x,n) \
+    ll x[n]; \
+    rep(i,0,n) cin>>x[i];
+#define inarrint(x,n) \
+    int x[n]; \
+    rep(i,0,n) cin>>x[i];
+
+#define instr(...) \
+    string __VA_ARGS__; \
+    read(__VA_ARGS__);
+#define inll(...) \
+    ll __VA_ARGS__; \
+    read(__VA_ARGS__);
+#define inint(...) \
+    int __VA_ARGS__; \
+    read(__VA_ARGS__);
+
+#define debug(arg) cerr<<__LINE__<<" "<<#arg<<" --> "<<arg<<endl;
+#define clr(a) memset(a,0,sizeof(a))
+#define lb lower_bound
+#define ub upper_bound
+#define setbits(x) __builtin_popcount(x)
+#define setbitsll(x) __builtin_popcountll(x)
+
+inline ll addmod(ll a,ll b){ a+=b; if(a>=mod) a-=mod; return a; }
+inline ll submod(ll a,ll b){ a-=b; if(a<0) a+=mod; return a; }
+inline ll mulmod(ll a,ll b){ return (a%mod)*(b%mod)%mod; }
+
+ll binExp(ll a,ll e,ll m=mod){
+    a%=m;
+    ll r=1;
+    while(e){
+        if(e&1) r=(r*a)%m;
+        a=(a*a)%m;
+        e>>=1;
+    }
+    return r;
+}
+
+ll modInv(ll a,ll m=mod){ return binExp(a,m-2,m); }
+
+ll gcdll(ll a,ll b){
+    while(b){
+        a%=b;
+        swap(a,b);
+    }
+    return a;
+}
+
+ll lcmll(ll a,ll b){
+    return a/gcdll(a,b)*b;
+}
+
+vll fact,invFact;
+
+void combi(int n){
+    fact.assign(n+1,1);
+    invFact.assign(n+1,1);
+    rep(i,1,n+1) fact[i]=mulmod(fact[i-1],i);
+    invFact[n]=modInv(fact[n]);
+    for(int i=n-1;i>=0;i--){
+        invFact[i]=mulmod(invFact[i+1],i+1);
+    }
+}
+
+ll ncr(ll n,ll r){
+    if(r<0||r>n) return 0;
+    return mulmod(fact[n],mulmod(invFact[r],invFact[n-r]));
+}
+
+vector<int> primes,lp;
+
+void sieve(int n=2000000){
+    lp.assign(n+1,0);
+    primes.clear();
+    for(int i=2;i<=n;++i){
+        if(!lp[i]){
+            lp[i]=i;
+            primes.pb(i);
+        }
+        for(int p:primes){
+            if(p>lp[i]||1LL*i*p>n) break;
+            lp[i*p]=p;
+        }
+    }
+}
+
+struct BIT{
+    int n;
+    vector<ll> bit;
+
+    BIT(int _=0){ init(_); }
+
+    void init(int _){
+        n=_;
+        bit.assign(n+1,0);
+    }
+
+    void add(int idx,ll val){
+        for(++idx;idx<=n;idx+=idx&-idx) bit[idx]+=val;
+    }
+
+    ll sumPrefix(int idx){
+        ll s=0;
+        for(++idx;idx;idx-=idx&-idx) s+=bit[idx];
+        return s;
+    }
+
+    ll sumRange(int l,int r){
+        return sumPrefix(r)-(l?sumPrefix(l-1):0);
+    }
+};
+
+template<typename T>
+struct SegTree{
+    int n;
+    vector<T> st;
+    T id;
+    function<T(T,T)> op;
+
+    SegTree(int _n,T _id,function<T(T,T)> _op):n(_n),id(_id),op(_op){
+        st.assign(2*n,id);
+    }
+
+    void build(const vector<T>& a){
+        rep(i,0,n) st[i+n]=a[i];
+        for(int i=n-1;i;--i) st[i]=op(st[i<<1],st[i<<1|1]);
+    }
+
+    void upd(int pos,T val){
+        for(st[pos+=n]=val;pos>1;pos>>=1) st[pos>>1]=op(st[pos],st[pos^1]);
+    }
+
+    T query(int l,int r){
+        T L=id,R=id;
+        for(l+=n,r+=n+1;l<r;l>>=1,r>>=1){
+            if(l&1) L=op(L,st[l++]);
+            if(r&1) R=op(st[--r],R);
+        }
+        return op(L,R);
+    }
+};
+
+struct Matrix{
+    int n;
+    vvll a;
+
+    Matrix(int _n=0,bool id=false):n(_n),a(n,vector<ll>(n,0)){
+        if(id) rep(i,0,n) a[i][i]=1;
+    }
+
+    Matrix operator*(const Matrix& o)const{
+        Matrix r(n);
+        rep(i,0,n){
+            rep(k,0,n){
+                if(a[i][k]){
+                    rep(j,0,n){
+                        r.a[i][j]=(r.a[i][j]+a[i][k]*o.a[k][j])%mod;
+                    }
+                }
+            }
+        }
+        return r;
+    }
+};
+
+Matrix matPow(Matrix base,ll exp){
+    Matrix res(base.n,true);
+    while(exp){
+        if(exp&1) res=res*base;
+        base=base*base;
+        exp>>=1;
+    }
+    return res;
+}
+
+struct custom_hash{
+    static uint64_t splitmix64(uint64_t x){
+        x+=0x9e3779b97f4a7c15;
+        x=(x^(x>>30))*0xbf58476d1ce4e5b9;
+        x=(x^(x>>27))*0x94d049bb133111eb;
+        return x^(x>>31);
+    }
+
+    size_t operator()(uint64_t x)const{
+        static const uint64_t FIXED_RANDOM=chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x+FIXED_RANDOM);
+    }
+};
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const vector<pair<int,int>> dir={{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+
+ll mergeMin(ll a,ll b){ return min(a,b); }
+ll mergeMax(ll a,ll b){ return max(a,b); }
+
+void buildMin(vll& arr,ll start,ll end,ll index,ll seg[]){
+    if(start==end){
+        seg[index]=arr[start];
+        return;
+    }
+    ll mid=(start+end)/2;
+    buildMin(arr,start,mid,2*index+1,seg);
+    buildMin(arr,mid+1,end,2*index+2,seg);
+    seg[index]=mergeMin(seg[2*index+1],seg[2*index+2]);
+}
+
+void buildMax(vll& arr,ll start,ll end,ll index,ll seg[]){
+    if(start==end){
+        seg[index]=arr[start];
+        return;
+    }
+    ll mid=(start+end)/2;
+    buildMax(arr,start,mid,2*index+1,seg);
+    buildMax(arr,mid+1,end,2*index+2,seg);
+    seg[index]=mergeMax(seg[2*index+1],seg[2*index+2]);
+}
+
+void updateMin(ll pos,ll val,ll start,ll end,ll index,ll seg[]){
+    if(start==end){
+        seg[index]=val;
+        return;
+    }
+    ll mid=(start+end)/2;
+    if(pos<=mid) updateMin(pos,val,start,mid,2*index+1,seg);
+    else updateMin(pos,val,mid+1,end,2*index+2,seg);
+    seg[index]=mergeMin(seg[2*index+1],seg[2*index+2]);
+}
+
+void updateMax(ll pos,ll val,ll start,ll end,ll index,ll seg[]){
+    if(start==end){
+        seg[index]=val;
+        return;
+    }
+    ll mid=(start+end)/2;
+    if(pos<=mid) updateMax(pos,val,start,mid,2*index+1,seg);
+    else updateMax(pos,val,mid+1,end,2*index+2,seg);
+    seg[index]=mergeMax(seg[2*index+1],seg[2*index+2]);
+}
+
+ll queryMin(ll l,ll r,ll start,ll end,ll index,ll seg[]){
+    if(r<start||end<l) return LLONG_MAX;
+    if(l<=start&&end<=r) return seg[index];
+    ll mid=(start+end)/2;
+    return mergeMin(
+        queryMin(l,r,start,mid,2*index+1,seg),
+        queryMin(l,r,mid+1,end,2*index+2,seg)
+    );
+}
+
+ll queryMax(ll l,ll r,ll start,ll end,ll index,ll seg[]){
+    if(r<start||end<l) return LLONG_MIN;
+    if(l<=start&&end<=r) return seg[index];
+    ll mid=(start+end)/2;
+    return mergeMax(
+        queryMax(l,r,start,mid,2*index+1,seg),
+        queryMax(l,r,mid+1,end,2*index+2,seg)
+    );
+}
+
+ll n;
+vvll adj;
+vll height, heavy;
+vll ans;
+vll arr;
+ll idx = 0;
+vector<ll*> dp;
+
+void dfs(int node,int p){
+    height[node] = 1;
+    heavy[node] = -1;
+    ll maxi = 0;
+    for(auto &v:adj[node]){
+        if(v==p) continue;
+        dfs(v,node);
+        if(height[v]+1>height[node]){
+            height[node] = height[v]+1;
+        }
+        if(height[v]>maxi){
+            maxi = height[v];
+            heavy[node] = v;
+        }
+    }
+}
+
+void f(int node,int p){
+    dp[node][0] = 1;
+    ans[node] = 0;
+    ll child = heavy[node];
+    if(child!=-1){
+        dp[child] = dp[node]+1;
+        f(child,node);
+        ans[node] = ans[child]+1;
+    }
+    for(auto &v:adj[node]){
+        if(v==p || v==child) continue;
+        dp[v] = &arr[idx];
+        idx = idx+height[v];
+        f(v,node);
+        for(int i=0;i<height[v];i++){
+            dp[node][i+1] += dp[v][i];
+            ll curr = ans[node];
+            if(dp[node][i+1]>dp[node][curr]){
+                ans[node] = i+1;
+            }
+            else if(dp[node][i+1]==dp[node][curr] && i+1<curr){
+                ans[node] = i+1;
+            }
+        }
+    }
+    if(dp[node][ans[node]]==1){
+        ans[node] = 0;
+    }
+}
+
+void solve(){
+    cin>>n;
+    adj.resize(n+1);
+    height.resize(n+1);
+    heavy.resize(n+1,-1);
+    ans.resize(n+1);
+    arr.resize(n+1,0);
+    dp.resize(n+1);
+    rep(i,0,n-1){
+        inll (u,v);
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    dfs(1,0);
+    dp[1] = &arr[idx];
+    idx = idx+height[1];
+    f(1,0);
+    rep(i,1,n+1){
+        print(ans[i]);
+    }
+
+}
+
+int main(){
+    fast;
+    solve();
+    return 0;
+}
